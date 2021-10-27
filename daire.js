fa55f924e446038e6ad104929bb6fd78eb3df100
Yusif Shikhseyidov <yusif1580@gmail.com>
2021-10-27 18:13:20 +0400

    first commit

---
+++ daire.js
@@ -0,0 +1,12 @@
+// function areaCircle(p, r) {
+//     console.log(p * r**2);
+// }
+
+// areaCircle(3.14, 2);
+
+
+// function areaD(){
+//     console.log(Math.PI * Math.floor(Math.random() * 10));
+// }
+
+// areaD();
